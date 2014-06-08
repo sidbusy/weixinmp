29dbe0e8b6effed057c17e2ee61d81dec09bdddc
sidbusy <isidbusy@gmail.com>
2014-06-08 17:34:49 +0800

    add file lock

--- accesstoken.go
+++ accesstoken.go
@@ -13,12 +13,23 @@ type AccessToken struct {
 	AppId     string
 	AppSecret string
 	TmpName   string
+	LckName   string
 }
 
 // get fresh access_token string
 func (this *AccessToken) Fresh() (string, error) {
 	if this.TmpName == "" {
 		this.TmpName = "accesstoken.tmp"
+	}
+	if this.LckName == "" {
+		this.LckName = this.TmpName + ".lck"
+	}
+	for {
+		if this.locked() {
+			time.Sleep(time.Second)
+			continue
+		}
+		break
 	}
 	fi, err := os.Stat(this.TmpName)
 	if err != nil && !os.IsExist(err) {
@@ -41,6 +52,10 @@ func (this *AccessToken) Fresh() (string, error) {
 }
 
 func (this *AccessToken) fetchAndStore() (string, error) {
+	if err := this.lock(); err != nil {
+		return "", err
+	}
+	defer this.unlock()
 	token, err := this.fetch()
 	if err != nil {
 		return "", err
@@ -52,10 +67,10 @@ func (this *AccessToken) fetchAndStore() (string, error) {
 }
 
 func (this *AccessToken) store(token string) error {
-	dir := path.Dir(this.TmpName)
-	fi, err := os.Stat(dir)
+	path := path.Dir(this.TmpName)
+	fi, err := os.Stat(path)
 	if os.IsNotExist(err) {
-		if err := os.MkdirAll(dir, os.ModeTemporary); err != nil {
+		if err := os.MkdirAll(path, os.ModeTemporary); err != nil {
 			return err
 		}
 	}
@@ -84,4 +99,32 @@ func (this *AccessToken) fetch() (string, error) {
 		return "", err
 	}
 	return rtn.AccessToken, nil
+}
+
+func (this *AccessToken) unlock() error {
+	return os.Remove(this.LckName)
+}
+
+func (this *AccessToken) lock() error {
+	path := path.Dir(this.LckName)
+	fi, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		if err := os.MkdirAll(path, os.ModeTemporary); err != nil {
+			return err
+		}
+	}
+	if !fi.IsDir() {
+		return errors.New("path is not a directory")
+	}
+	lck, err := os.Create(this.LckName)
+	if err != nil {
+		return err
+	}
+	lck.Close()
+	return nil
+}
+
+func (this *AccessToken) locked() bool {
+	_, err := os.Stat(this.LckName)
+	return !os.IsNotExist(err)
 }
